618e26d55022f1a335248438f123c06cab128413
Tvoje Meno <tvoj@email.com>
2025-12-21 17:16:39 +0100

    FIX: Pridanie vsetkych ios suborov a odstranenie stareho configu

--- capacitor.config.ts
+++
@@ -1,9 +0,0 @@
-import type { CapacitorConfig } from '@capacitor/cli';
-
-const config: CapacitorConfig = {
-  appId: 'com.idealtwin.app',
-  appName: 'IdealTwin',
-  webDir: 'dist'
-};
-
-export default config;
